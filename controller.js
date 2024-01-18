e6decccb9099b887ea6a1e143b8c1275da14194d
ahmadharis-ux <ahmadfreedom27@gmail.com>
2024-01-18 13:24:46 +0700

    berhasil menampilkan semua data dan perId

--- controller.js
+++ controller.js
@@ -3,6 +3,29 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req, res){
-    response.ok("Aplikasi Res Api berjalan", res);
+exports.index = function (req, res) {
+  response.ok('Aplikasi Res Api berjalan', res);
+};
+
+// Menampilkan Semua data User
+exports.getAllUser = function (req, res) {
+  connection.query('SELECT * FROM users', function (error, rows, fileds) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
+
+// user by Id
+exports.userById = function (req, res) {
+  let id = req.params.id;
+  connection.query('SELECT *FROM users where id = ?', [id], function (error, rows, fields) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
 };
--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ module.exports = function (app) {
   var jsonku = require('./controller');
 
   app.route('/').get(jsonku.index);
+  app.route('/alldata').get(jsonku.getAllUser);
+  app.route('/data/:id').get(jsonku.userById);
 };
