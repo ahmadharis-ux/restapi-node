01a64d51de85407749bcec9bc05eb0a9352f9b1a
ahmadharis-ux <ahmadfreedom27@gmail.com>
2024-01-18 11:50:32 +0700

    menambahkan kode res

---
+++ res.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.ok = function(values, res){
+    var data = {
+        'status':200,
+        'values':values,
+        'message':'OK'
+    };
+
+     res.json(data);
+     res.end();
+}
